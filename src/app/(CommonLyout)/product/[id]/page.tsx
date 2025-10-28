15888d78d6a154af281102a994963c21352d416f
Suyel Haque <slsuyel@gmail.com>
2025-10-28 14:40:46 +0600

    fix: type issue resolved

--- src/app/(CommonLyout)/product/[id]/page.tsx
+++ src/app/(CommonLyout)/product/[id]/page.tsx
@@ -1,13 +1,9 @@
 import ProductDetails from "@/components/Pages/Others/ProductDetails";
 import { TProduct } from "@/lib/types";
 
-interface PageProps {
-  params: { id: string };
-}
-
 export const dynamic = "force-dynamic";
 
-export default async function ProductPage({ params }: PageProps) {
+export default async function ProductPage({ params }: any) {
   const res = await fetch(`https://fakestoreapi.com/products/${params.id}`);
   const product: TProduct = await res.json();
 
